73f164b760774f7e8a466827460525e31c7c8f25
ScrabbleScratch <55005341+ScrabbleScratch@users.noreply.github.com>
2021-12-15 11:46:32 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+docker stop AnimeScrapper && docker rm AnimeScrapper
+docker stop CharacterScrapper && docker rm CharacterScrapper
+docker stop MangaScrapper && docker rm MangaScrapper
 docker rmi anime-scrapper:mqtt character-scrapper:mqtt manga-scrapper:mqtt
 docker build -t anime-scrapper:mqtt AnimeScrapper
 docker build -t character-scrapper:mqtt CharacterScrapper
