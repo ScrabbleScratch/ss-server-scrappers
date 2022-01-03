f618ad1439cd5cf83a45fc4de5dc004e3ff1c430
ScrabbleScratch <55005341+ScrabbleScratch@users.noreply.github.com>
2022-01-03 16:17:32 -0600

    Update anime-builder.sh

--- builders/anime-builder.sh
+++ builders/anime-builder.sh
@@ -1,7 +1,1 @@
-docker run -dit \
-    --name=AnimeScrapper \
-    --restart=unless-stopped \
-    --network=host \
-    -v /mnt/DISk/AppData/AnimeScrapper/config:/scrapper/config \
-    -v /mnt/DISk/AppData/AnimeScrapper/logs:/scrapper/logs \
-    anime-scrapper:mqtt
+docker run -dit --name=AnimeScrapper --restart=unless-stopped --network=host -v /mnt/DISk/AppData/AnimeScrapper/config:/scrapper/config -v /mnt/DISk/AppData/AnimeScrapper/logs:/scrapper/logs anime-scrapper:mqtt
