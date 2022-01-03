ab9830fcb24ab1c081b49be0ee724abb5bb543b4
ScrabbleScratch <55005341+ScrabbleScratch@users.noreply.github.com>
2022-01-03 16:23:30 -0600

    Delete builders directory

--- builders/anime-builder.sh
+++
@@ -1,1 +0,0 @@
-docker run -dit --name=AnimeScrapper --restart=unless-stopped --network=host -v /mnt/DISk/AppData/AnimeScrapper/config:/scrapper/config -v /mnt/DISk/AppData/AnimeScrapper/logs:/scrapper/logs anime-scrapper:mqtt
--- builders/character-builder.sh
+++
@@ -1,1 +0,0 @@
-docker run -dit --name=CharacterScrapper --restart=unless-stopped --network=host -v /mnt/DISk/AppData/CharacterScrapper/config:/scrapper/config -v /mnt/DISk/AppData/CharacterScrapper/logs:/scrapper/logs character-scrapper:mqtt
