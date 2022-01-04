c389aab693490504c868af056757d72ff2060b39
ScrabbleScratch <55005341+ScrabbleScratch@users.noreply.github.com>
2022-01-03 18:19:50 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@ docker stop AnimeScrapper && docker rm AnimeScrapper
 docker stop CharacterScrapper && docker rm CharacterScrapper
 docker stop MangaScrapper && docker rm MangaScrapper
 docker rmi anime-scrapper character-scrapper manga-scrapper
-docker rmi anime-scrapper:mqtt character-scrapper:mqtt manga-scrapper:mqtt
 docker build -t anime-scrapper AnimeScrapper
 docker build -t character-scrapper CharacterScrapper
 docker build -t manga-scrapper MangaScrapper
